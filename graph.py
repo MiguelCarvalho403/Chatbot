1d6091ac20abe24275bbe1c4236a84b12d329f68
MiguelDCarvalho <alvescarvalho.miguel403@gmail.com>
2026-03-03 18:04:15 -0300

    Agora modelos são passados como parametro

--- graph.py
+++ graph.py
@@ -2,7 +2,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 from langgraph.graph import StateGraph, START, END
 from langgraph.types import interrupt, Command
-from langgraph.prebuilt import ToolNode # Classe que contem tools
+from langgraph.prebuilt import ToolNode # Classe que chama tools
 from langgraph.graph.message import add_messages
 from langgraph.checkpoint.memory import InMemorySaver
 # add_message é uma reduce_function, função que dita como um dado será modificado ao ser atualizado
@@ -13,6 +13,7 @@ from langchain_core.messages import ToolMessage   # Classe que gerencia messagen
 from langchain_core.messages import HumanMessage
 from langchain_core.messages import AIMessage
 from langchain_core.messages import SystemMessage # Classe que gerencia messagens de sistema, ordens a LLM
+from langchain_core.runnables import RunnableConfig
 
 from typing import TypedDict, Dict
 from typing import Annotated # Adiciona decrição a variaveis, variavel = Annotaded[tipo_variavel, "descrição"]
@@ -22,23 +23,50 @@ from tools.tools import search_resources
 from llm import LLM
 from utils import read_yaml
 
-import torch
-
 from icecream import ic
 
 # Herda da classe TypedDict, permite declarar tipos para classe
-# Esta classe serve para gerenciar o estdo atual do agente
+# Esta classe serve para gerenciar o estado atual do agente
 ''' Ver mais informações nas importações de cada função/classe''' 
+# Esse tipo de classe não deve ser instanciada, serve como um blueprint para criação e gerenciamento de dicionarios
 class AgentState(TypedDict):
     messages: Annotated[Sequence[BaseMessage], add_messages] # Troca de messagens entre chat e usuário
     gov_resources: Annotated[Sequence[ToolMessage], add_messages] # Recursos do governo
     # tipo da variavel messages: sequence[BaseMessages]
     # conteudo: add_messages
 
-def llm_call(state: AgentState):
+def init_state(state: AgentState, config: RunnableConfig):
+    print('Inicializando...')
+    models = config.get('configurable')
+    config.get('configurable').update({'llm_wrapper':LLM(model=models['llm'], tokenizer=models['tokenizer'])})
+    return {'messages': [],
+            'gov_resources': [ToolMessage(content="", tool_call_id="init")]}
+
+import uuid
+def messages_router(func_name: str, result: BaseMessage)->dict:
+    if func_name in ["search_resources"]:
+        return {'gov_resources': [ToolMessage(content=result, tool_call_id=uuid.uuid4())]}
+
+def llm_call(state: AgentState, config: RunnableConfig):
+    llm = config['configueable']['llm_wrapper']
     system_message = SystemMessage(content='Você é uma assitente que responde sobre dados governamentais do governo brasileiro, responda conforme o requisitado')
-    response = llm.invoke([system_message] + state['messages'], tools)
+    response = llm.invoke([system_message] + state['messages'] + [state['gov_resources'][-1]], tools)
     return {'messages': [response]}
+
+def tool_call(state: AgentState):
+    # Tipo AIMessage(content:str, tool_calls:dict('name': 'func_name', 'args': {}, 'id': 'id'), additional_kwargs:dict)
+    ai_message = state['messages'][-1]
+    '''OBS: Por enquanto utlizaremos tool_calls[0], permitindo a chamada de apenas uma tool por vez'''
+    func_name = ai_message.tool_calls[0].get('name')
+    func = globals()[func_name]
+    args = ai_message.tool_calls[0].get('args')
+    result= None
+    
+    if callable(func):
+        result = func(**args)
+        return messages_router(func_name, result)
+    
+    return {'messages': [SystemMessage(content="Função retornada não existe")]}
 
 def should_continue(state: AgentState)-> AgentState:
     messages = state['messages']
@@ -48,6 +76,15 @@ def should_continue(state: AgentState)-> AgentState:
     else:
         return 'user_input_node' 
 
+def kill_robot(state: AgentState):
+
+    last_message = state['messages'][1].content
+
+    if last_message.lower() == "hasta la vista":
+        return 'hasta la vista baby'
+    else:
+        return 'continue'
+
 def user_input(state: AgentState)-> AgentState:
     '''Entrada do usuário'''
 
@@ -55,27 +92,29 @@ def user_input(state: AgentState)-> AgentState:
     
     return {'messages': [HumanMessage(content=user_input)]}
 
-def chat_graph():
 
-    #def tool_node():
-    #    result = tool_call.invoke(AgentState)
-#
- #       return {'messages': "asd",
-  #              'gov_resources': result}
+def chat_graph():
 
     graph = StateGraph(AgentState)
 
     graph.add_node('user_input_node', user_input)
     graph.add_node('llm_call_node', llm_call)
 
-    tool_node = ToolNode(tools=tools) # Objeto que executa ferramentas
+    #tool_node = ToolNode(tools=tools) # Objeto que executa ferramentas
     # OBS: O objeto ToolNode sempre irá guardar o retorno da ferramenta onde houver uma chamada de ferramenta
     # Nesse caso na variável messages, por isso é recomendavel criar um wrapper para desviar o conteúdo
     # da ferramenta para outra variável a fim de não sobrecarregar a llm durante a inferência
 
-    graph.add_node('tools_node', tool_node) # Adiciona objeto que contém tools em um nó
+    graph.add_node('tools_node', tool_call) # Adiciona objeto que contém tools em um nó
 
-    graph.set_entry_point('llm_call_node')
+    #graph.set_entry_point('llm_call_node')
+
+    graph.add_node('init_state_node', init_state)
+
+    graph.set_entry_point('init_state_node')
+    #graph.add_edge(START, 'init_state_node')
+
+    graph.add_edge('init_state_node', 'llm_call_node')
 
     graph.add_conditional_edges(
         source='llm_call_node', # determina a partir de qual nó haverá arestas condicionais   
@@ -87,35 +126,31 @@ def chat_graph():
             'user_input_node': 'user_input_node',
         }
     )
-    '''Adicionar conditional edge, se ultima tool for final answer devolver resposta ao usuário'''
+
     graph.add_edge('tools_node', 'llm_call_node') # Aresta que retorna ao agente, criando uma conexão circular
-    graph.add_edge('user_input_node', 'llm_call_node')
+    
+    #graph.add_edge('user_input_node', 'llm_call_node')
+
+    graph.add_conditional_edges(
+        source='user_input_node',
+        path=kill_robot,
+        path_map=
+        {   
+            'hasta la vista baby': END,
+            'continue': 'llm_call_node'
+        }
+    )
 
     checkpointer = InMemorySaver()  
     app = graph.compile(checkpointer=checkpointer)
 
     return app
 
-def teste():
-
-    #my_state = AgentState()
-    #my_state.messages = {'messages': [AIMessage(content="asdad",
-    #                               tool_call=[{'name': search_resources,
-    #                                           'Args': {'query': "infecções"},
-    #                                           'id': "asd43"}])]}
-    
-    #tool_call = ToolNode(tools)
-    #result = tool_call.invoke(state)
-
-    pass
-
 # ============================= Carregando modelo =============================
 
 tools = [search_resources] #, Final_answer]
-model_name = ["Qwen/Qwen3-0.6B", "Qwen/Qwen2.5-0.5B-Instruct"]
-model_name = model_name[0]
-
-llm = LLM()
+#model_name = ["Qwen/Qwen3-0.6B", "Qwen/Qwen2.5-0.5B-Instruct"]
+#model_name = model_name[0]
 
 if __name__ == '__main__':
-    teste()
+    pass
